57ed636674ce00cf18323ca0ac772c0e09edb2ed
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-02-12 20:20:59 +0500

    increment and decrement done using useState

--- src/Pat.jsx
+++ src/Pat.jsx
@@ -2,12 +2,18 @@ import { useState,React } from "react";
 const Pat=()=>{
     let [count,setcount]= useState(0);
     function display(){
+        
      setcount(count+1);
+    }
+    function decrement(){
+        if(count>0)
+        setcount(count-1)
     }
     return(
     <div>
         <h1>Button clicked {count} time</h1>
-        <button onClick={display}>Click me</button>
+        <button onClick={display}>increment</button>
+        <button onClick={decrement}>Decrement</button>
     </div>
     )
 }
