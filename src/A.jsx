07ee275826d68ad77a5a7a43b7393c6b19b300ee
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-02-15 12:17:05 +0500

    Context API pass two value

--- src/A.jsx
+++ src/A.jsx
@@ -1,14 +1,17 @@
 import B from "./B";
 import React,{createContext} from "react"
 const data=createContext()
+const data1=createContext();
 function A(props){
     const name="Rana Abobakar"
+    const gender="Male"
     return (
 <data.Provider value={name}>
-
+<data1.Provider value={gender}>
 <B />
+</data1.Provider>
 </data.Provider>
     )
 }
 export default A;
-export {data};
+export {data,data1};
--- src/C.jsx
+++ src/C.jsx
@@ -1,14 +1,20 @@
 import React from "react"
-import {data} from "./A"
+import {data,data1} from "./A"
 function C(props){
     return (
         <data.Consumer>
 {
     (name)=>{
         return (
-            <div>
-                <h1>context api done by {name}</h1>
-            </div>
+           <data1.Consumer>
+         {   (gender)=>{
+                return (
+                    <div>
+                        <h1>My name is {name} and my gender is {gender}</h1>
+                        </div>
+                )
+            }}
+           </data1.Consumer>
         )
     }
 }
