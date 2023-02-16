7dc3e2e86a137d4f06a4be558a9e74f07e5811b5
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-02-16 11:56:21 +0500

    useMemo done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import A from "./A";
-// import Pat from "./Pat";
+import React,{useMemo, useState } from "react";
+
+
+
 
 function App(data) {
+
+  const [add,setadd]=useState(0)
+  const [minux,setminux]=useState(100)
+const multiplication=useMemo(()=>{
+  console.log(add)
+  return add*10
+},[add])
   return (
     <>
      <h1>hello</h1>
-      <A/>
+     
+     <span>{multiplication}</span>
+     <br/>
+      <button onClick={()=>{setadd(add+1)}}>add</button>
+      <span>{add}</span>
+    <br/>
+      <button onClick={()=>setminux(minux-1)}>subtraction</button>
+      <span>{minux}</span>
+     
     </>
   );
 }
 export default App;
+
+// usememo store value of a function .usememo have two parameter first for function second for dependence 
--- src/B.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import C from './C'
-function B(props){
-    return (
-<C />
-    )
-}
-export default B;
