bc69539dee6cb3915eabf9ed22638c41e101ef71
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-02-22 12:24:43 +0500

    form registration using video with  useEffect

--- src/childA.jsx
+++ src/childA.jsx
@@ -1,26 +1,28 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import "./style.css"
 function ChildA(){
   const data={name:"",mail:"",password:""}
     const [inputdata,setinputdata]=useState(data)
-
+const [check,setcheck]=useState(false)
     function handleinput(e){
      setinputdata({...inputdata,[e.target.name]:e.target.value})
      console.log(inputdata)
     }
-    
+    useEffect(()=>{
+      console.log("i am set")
+    },[check])
     function submitdata(e){
         e.preventDefault(); // use for prevent html default behavior
    if(!inputdata.name|| !inputdata.mail||!inputdata.password){
     alert("fill all input")
    }
    else{
-
+setcheck(true)
    }
        }
         return(
           <div id="farmdata">
-        {(inputdata.name&&inputdata.mail&&inputdata.password)?<h1 id='r1'>{inputdata.name} Register successfully</h1>:<></>}
+        {check?<h1 id='r1'>{inputdata.name} Register successfully</h1>:<></>}
       <div id="div2">
           <form onSubmit={submitdata} id='farmdata'>
            <h1 id='r1'>Registration form </h1>
