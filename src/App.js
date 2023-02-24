3e269d580eff1c9b5a35d8b0a32dfc74863d6169
RANA ABOBAKAR <ranaabobakarit@gmail.com>
2023-02-24 05:21:26 +0500

    error: click add button but remove function call along with

--- src/App.js
+++ src/App.js
@@ -1,32 +1,56 @@
-import React,{useCallback, useState } from "react";
-import ChildA from "./ChildA";
+import React,{  useState } from "react";
 
 import "./style.css"
 function App(){
-
-
-
-function App(data) {
-
-  const [add,setadd]=useState(0)
-  const [count,setcount]=useState(0)
-
-const multiplication=useCallback(()=>{
+const [listvalue,setlistvalue]=useState([])
+const [v,setv]=useState('')
+function control_list(e){
+setv(e.target.value)
  
-},[count])
-  return (
-    <>
-     <h1>hello</h1>
-<ChildA count={multiplication}/>
+}
+
+function submit_btn(){
+  console.log(v)
+  if(v){
     
-     <br/>
-      <h1>{add}</h1>
-      <button onClick={()=>{setadd(add+1)}}>add</button>
-      <br/>
-      <h1>{count}</h1>
-      <button onClick={()=>{setcount(count+1)}}>count</button>
-   
-    </>
+    setlistvalue([...listvalue,{
+      id:listvalue.length,
+      value:v
+    }])
+  }
+  else{
+    alert("fill input")
+  }
+}
+
+function remove_value(v)
+{
+  console.log("rmove "+v)
+  const vv =listvalue.map(v1=>{
+   return v1.id!==v
+  })
+
+  setlistvalue([vv]);
+}
+
+  return (
+   <>
+   <div style={{width:"65%",backgroundColor:"#06054b",textAlign:"center",margin:"auto"}}>
+    <div style={{alignItems:'center',backgroundColor:"#06054b"}}>
+<h1 style={{color:"white"}}>ToDo List</h1>
+<div >
+ {listvalue.map(v=>{
+  return <> <h3 key={v.id} style={{display:"inline",backgroundColor:"#9fc8f0",color:"black", padding:"4px",borderRadius:"3px"}}>{v.value} {v.id}</h3>
+  <button onClick={remove_value(v.id)} style={{margin:"10px",backgroundColor:"#479f51",padding:"5px",borderRadius:"3px"}}>Remove</button><br/></>
+ })}
+</div>
+<div>
+  <input style={{display:"inline"}} name="list" value={v} onChange={control_list}></input>
+  <button style={{margin:"10px",backgroundColor:"#479f51"}} onClick={submit_btn}>Add List</button>
+</div>
+    </div>
+   </div>
+   </>
   );
 }
 export default App;
